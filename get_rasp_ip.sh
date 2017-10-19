b430d16e9d3abcbd90c725783cf6f60440d870cb
fengqi <feng.qi@msn.com>
2017-10-19 15:24:04 +0800

    update

--- get_rasp_ip.sh
+++ get_rasp_ip.sh
@@ -11,7 +11,10 @@ pi_ip="$(arp -n | grep -i b8:27:eb | awk '{print $1}')"
 
 if [ -z ${pi_ip} ]; then
     # if arp table doesn't contain info of Pi, then use nmap
-    pi_ip="$(sudo nmap -sP 192.168.1.0/24 | awk '/^Nmap/{ip=$NF}/B8:27:EB/{print ip}')"
+    subnet_mask="$(hostname -I | tr ' ' '\n' | grep 192.168 | awk -F. '{print $1 "." $2 "." $3;}').0/24"
+    # echo ${subnet_mask}
+    # pi_ip="$(sudo nmap -sP 192.168.1.0/24 | awk '/^Nmap/{ip=$NF}/B8:27:EB/{print ip}')"
+    pi_ip="$(sudo nmap -sP ${subnet_mask} | awk '/^Nmap/{ip=$NF}/B8:27:EB/{print ip}')"
 fi
 
 echo "${pi_ip}"
